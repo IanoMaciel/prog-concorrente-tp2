b4f9deeadb7e7d40b5a221b13163b33bd4b1dd6a
Iano Maciel <ianomaciel6385@gmail.com>
2021-11-17 21:35:58 -0400

    Q2: finalizada

---
+++ Q2.c
@@ -0,0 +1,3 @@
+ A variável cont_primo deve ser privada, pois ela é usada apenas dentro de
+ uma mesma iteração. Já a variável soma passa por uma redução, acumulando um
+ somatório para contar a quantidade total de primos.
